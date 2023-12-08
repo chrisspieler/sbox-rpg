5209af026ed3333c5e661cab63f3510ec76bad18
ducc <spielerc@gmail.com>
2023-12-08 04:06:06 -0700

    Set StartupScene, reduce Program.cs to being a place where we put global usings

--- code/Program.cs
+++ code/Program.cs
@@ -2,21 +2,3 @@
 global using System;
 global using System.Collections.Generic;
 global using System.Threading.Tasks;
-using Sandbox;
-
-
-public static class Program
-{
-	public static void Main()
-	{
-		//
-		// In the future this won't exist, we'll have an option for "default scene"
-		// and when you press play it'll load that.
-		//
-
-		GameManager.ActiveScene = new Scene();
-		GameManager.ActiveScene.LoadFromFile( "scenes/test_movement.scene" );
-
-		GameManager.IsPlaying = true;
-	}
-}
