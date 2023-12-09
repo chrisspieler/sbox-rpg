8678dd4ac856bf14ad3a8a20019a82a8f45e0c7b
ducc <spielerc@gmail.com>
2023-12-09 03:41:19 -0700

    Add Looker component, some colliders on the player

---
+++ code/Components/Looker.cs
@@ -0,0 +1,24 @@
+﻿namespace Sandbox;
+
+// If you say the name of this class in Jonathan Blow's Twitch chat, he will ban you.
+public class Looker : Component
+{
+	[Property] public CitizenAnimation Animator { get; set; }
+	[Property] public TriggerCollectorComponent TriggerCollector { get; set; }
+
+	protected override void OnUpdate()
+	{
+		var collisions = TriggerCollector.GetCollisions();
+		if ( !collisions.Any() )
+		{
+			Animator.LookAtObject = null;
+			return;
+		}
+
+		var closest = collisions
+			.Select( collider => (collider, Distance: Vector3.DistanceBetween( Transform.Position, collider.Transform.Position )) )
+			.OrderBy( x => x.Distance )
+			.First();
+		Animator.LookAtObject = closest.collider.GameObject;
+	}
+}
